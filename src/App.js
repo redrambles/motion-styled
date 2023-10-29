18c58a673af757deb007b13130d95de3da1639a2
Ann Cascarano <4411121+redrambles@users.noreply.github.com>
2023-10-29 11:29:15 -0400

    Initial setup plus globalstyles

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+// import GlobalStyle from './globalStyles';
+// import Home from './pages/Home';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// import { Navbar, Footer } from './components';
+
+function App() {
+  return (
+    <Router>
+      <h1>YO BIATCH</h1>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/data/FeaturesData.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { BsPhone } from "react-icons/bs";
+import { GiConcentrationOrb } from "react-icons/gi";
+import { IoLogoXing } from "react-icons/io";
+import { HiCode } from "react-icons/hi";
+import { MdSlowMotionVideo } from "react-icons/md";
+import { FaPhotoVideo } from "react-icons/fa";
+
+const iconStyle = (Icon, size = "3rem") => <Icon color='#fefefe' size={size} />;
+
+export const featuresData = [
+  {
+    name: "UI/UX",
+    description: "Landing Pages, User Flow, Wireframing, Prototyping, Mobile App Design, Web App",
+    icon: iconStyle(BsPhone, "2rem"),
+    imgClass: "one"
+  },
+  {
+    name: "Icon/Illustration",
+    description: "Character Design, Icon Set, Illustration Guide, Illustration Set",
+    icon: iconStyle(GiConcentrationOrb),
+    imgClass: "two"
+  },
+  {
+    name: "Branding",
+    description: "Visual Identity, Stationary Kit, Marketing Materials",
+    icon: iconStyle(IoLogoXing),
+    imgClass: "three"
+  },
+  {
+    name: "Development",
+    description: "HTML/CSS, JavaScript Animation, WordPress, Responsive Website",
+    icon: iconStyle(HiCode),
+    imgClass: "four"
+  },
+  {
+    name: "Motion",
+    description: "2D Animation, UI Motion",
+    icon: iconStyle(MdSlowMotionVideo),
+    imgClass: "five"
+  },
+  {
+    name: "Photography",
+    description: "Travel Photography, Product Photography",
+    icon: iconStyle(FaPhotoVideo),
+    imgClass: "six"
+  }
+];
